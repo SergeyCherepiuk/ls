0b9b1808188211ddf17c0cb29ade785ff4a101a9
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-10-23 21:56:47 +0200

    Tree-like structure

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,17 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let paths: Vec<String> = args[1..].to_vec();
     let mut paths: VecDeque<String> = VecDeque::from(paths);
-    list_dirs(&mut paths);
+
+    while !paths.is_empty() {
+        let path = match paths.pop_front() {
+            Some(path) => path,
+            None => break,
+        };
+        list_dir(path, "    ", 0);
+    }
 }
 
-fn list_dirs(dirs: &mut VecDeque<String>) {
-    let path = match dirs.pop_front() {
-        Some(path) => path,
-        None => return,
-    };
-
+fn list_dir(path: String, indent_symbol: &str, indent_offset: usize) {
     // TODO: Check if "path" is a directory
     // TODO: Trim leading slashes
 
@@ -22,7 +24,8 @@ fn list_dirs(dirs: &mut VecDeque<String>) {
         Err(_) => return,
     };
 
-    println!("D: {path}:");
+    let indent = indent_symbol.repeat(indent_offset);
+    println!("{indent}D: {path}:");
 
     for entry in dir {
         let entry = match entry {
@@ -42,16 +45,13 @@ fn list_dirs(dirs: &mut VecDeque<String>) {
 
         if filetype.is_dir() {
             let dirpath = String::from(format!("{path}/{filename}"));
-            dirs.push_back(dirpath);
+            list_dir(dirpath, indent_symbol, indent_offset + 1);
             continue;
         }
 
         match entry.file_name().into_string() {
-            Ok(filename) => println!("F: {filename}"),
+            Ok(filename) => println!("{indent}{indent_symbol}F: {filename}"),
             Err(_) => continue,
         };
     }
-    println!();
-
-    return list_dirs(dirs);
 }
