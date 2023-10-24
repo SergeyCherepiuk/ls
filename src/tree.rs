5a7dd3db0a7aeccfcbb1d2fde12cec8f09eb5b46
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-10-24 17:51:30 +0200

    Files' permissions information

--- src/tree.rs
+++ src/tree.rs
@@ -1,7 +1,11 @@
 use std::fs;
 
+const INDENT_SYMBOL: &str = "\t";
 const DIR_SIGN: &str = "[D]";
 const FILE_SIGN: &str = "[F]";
+const RO_SIGN: &str = "(ro)";
+const RW_SIGN: &str = "(rw)";
+const UNKNOWN_SIGN: &str = "(unknown)";
 
 pub fn list_dir(dir: &str, indent_depth: usize) {
     let dir = dir.trim_end_matches('/');
@@ -10,8 +14,8 @@ pub fn list_dir(dir: &str, indent_depth: usize) {
         return;
     }
 
-    let dir_indent = "\t".repeat(indent_depth);
-    let file_indent = "\t".repeat(indent_depth + 1);
+    let dir_indent = INDENT_SYMBOL.repeat(indent_depth);
+    let file_indent = format!("{dir_indent}{INDENT_SYMBOL}");
 
     println!("{dir_indent}{DIR_SIGN} {dir}:");
 
@@ -32,11 +36,22 @@ pub fn list_dir(dir: &str, indent_depth: usize) {
             None => continue,
         };
 
-        if dir_entry.metadata().is_ok_and(|metadata| metadata.is_dir()) {
+        let metadata = dir_entry.metadata();
+        if (&metadata.as_ref()).is_ok_and(|metadata| metadata.is_dir()) {
             let dir_path = format!("{dir}/{file_name}");
             list_dir(dir_path.as_str(), indent_depth + 1);
         } else {
-            println!("{file_indent}{FILE_SIGN} {file_name}");
+            let permissions = match metadata {
+                Ok(metadata) => {
+                    if metadata.permissions().readonly() {
+                        RO_SIGN
+                    } else {
+                        RW_SIGN
+                    }
+                }
+                Err(_) => UNKNOWN_SIGN,
+            };
+            println!("{file_indent}{FILE_SIGN} {permissions} {file_name}");
         }
     }
 }
